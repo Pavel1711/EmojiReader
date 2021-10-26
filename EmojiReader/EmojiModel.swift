229e83f4de0f3a73a4a58ef6f5f8cd2ce1a7c789
Pavel1711 <45904265+Pavel1711@users.noreply.github.com>
2021-10-26 23:07:03 +0300

    Разработал весь функционал приложения

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by INFOLABS on 26.10.2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavorite: Bool
+    
+}
